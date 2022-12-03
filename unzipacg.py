dffa381cb6cb6399cd8326a3bb7e3ed280808a69
Rickenbacker620 <1057558227@qq.com>
2022-12-03 14:44:33 +0800

    unzipacg

---
+++ unzipacg.py
@@ -0,0 +1,29 @@
+import os
+import zipfile
+from zipfile import ZipFile
+import re
+
+
+def extract_link(file: ZipFile):
+    for old_name in file.namelist():
+        new_name = old_name.encode('cp437').decode('gbk')
+        if "百度网盘" in new_name:
+            print("-"*24 + new_name + "-"*24)
+            content = file.read(old_name).decode("utf-8")
+            reg = re.compile(r'(?<=备用百度秒传链接：\s)((.|\n)*)(?=\s+-+\s+注意)')
+            return reg.search(content).group(0).strip().split()
+
+
+links_all = []
+
+for root, dirs, files in os.walk("zipfiles"):
+    for file in files:
+        filepath = os.path.join(root, file)
+        zf = zipfile.ZipFile(filepath, "r")
+        links = extract_link(zf)
+        print(links)
+        links_all += links
+
+with open("result.txt", "w") as file:
+    for link in links_all:
+        file.write(link+"\n")
